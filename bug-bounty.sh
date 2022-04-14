114d88e438b104938f38d242d9b88fcf3ded28ab
Gajendrsingh7748 <80196896+Gajendrsingh@users.noreply.github.com>
2022-04-14 11:53:51 +0530

    Update bug-bounty.sh

--- bug-bounty.sh
+++ bug-bounty.sh
@@ -1,3 +1,7 @@
 chmod +x bug
 sudo mv bug /bin
+sudo apt install -y nmap uniscan lbd nikto dnsmap amass whatweb wapiti golismero dmitry  davtest dirb xsser fierce dnsenum  wafw00f theHarvester dnsrecon sslyze  
+git clone https://github.com/golismero/golismero.git
+cd golismero
+pip install -r requirements.txt
 echo "now you type bug and run tool"
